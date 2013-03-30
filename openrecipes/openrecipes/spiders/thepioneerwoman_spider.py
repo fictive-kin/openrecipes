7d3ca0a4fce6c1f1c30a0697fba14dfe3ab780a2
Ed Finkler <coj@funkatron.com>
2013-03-30 14:04:19 -0400

    remove some unnecessary comments

--- openrecipes/openrecipes/spiders/thepioneerwoman_spider.py
+++ openrecipes/openrecipes/spiders/thepioneerwoman_spider.py
@@ -1,8 +1,3 @@
-# This package will contain the spiders of your Scrapy project
-#
-# Please refer to the documentation for information on how to create and manage
-# your spiders.
-
 from scrapy.contrib.spiders import CrawlSpider, Rule
 from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 from scrapy.selector import HtmlXPathSelector
