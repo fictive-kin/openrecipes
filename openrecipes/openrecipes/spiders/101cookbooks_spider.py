c35caf4f8784e6b5a0c4768d59aa043db6ed0a0e
Ed Finkler <coj@funkatron.com>
2013-03-30 01:13:39 -0400

    initial stab at 101cookbooks spider

---
+++ openrecipes/openrecipes/spiders/101cookbooks_spider.py
@@ -0,0 +1,68 @@
+# This package will contain the spiders of your Scrapy project
+#
+# Please refer to the documentation for information on how to create and manage
+# your spiders.
+
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
+from scrapy.selector import HtmlXPathSelector
+from openrecipes.items import RecipeItem
+
+
+class OnehundredonecookbookscrawlSpider(CrawlSpider):
+    name = "101cookbooks.com"
+    allowed_domains = ["101cookbooks.com"]
+    start_urls = [
+        "http://www.101cookbooks.com/archives.html",
+    ]
+
+    rules = (
+        Rule(SgmlLinkExtractor(allow=('archives/.+\.html')),
+             callback='parse_item'),
+    )
+
+    def parse_item(self, response):
+        hxs = HtmlXPathSelector(response)
+
+        base_path = """//div[@id="recipe"]"""
+
+        recipes_scopes = hxs.select(base_path)
+
+        name_path = 'h1/text()'
+        description_path = '//meta[@property="og:description"]/@content'
+        url_path = '//meta[@property="og:url"]/@content'
+        image_path = '//meta[@property="og:image"][1]/@content'
+        prepTime_path = './/span[@class="preptime"]/span[@class="value-title"]/@title'
+        cookTime_path = './/span[@class="cooktime"]/span[@class="value-title"]/@title'
+
+        # super inconsistent in how the yield is formatted
+        recipeYield_path = "|".join([
+                                    '//div[@id="recipe"]/p[starts-with(i,"Makes")]/i',
+                                    '//div[@id="recipe"]/p[starts-with(i,"Serves")]/i',
+                                    '//div[@id="recipe"]/p[starts-with(em,"Makes")]/em',
+                                    '//div[@id="recipe"]/p[starts-with(em,"Serves")]/em',
+                                    '//div[@id="recipe"][starts-with(p,"Makes")]/p',
+                                    '//div[@id="recipe"][starts-with(p,"Serves")]/p',
+                                    ])
+        ingredients_path = 'blockquote/*'
+        datePublished = '//span[@class="published"]/span[@class="value-title"]/@title'
+
+        recipes = []
+        for r_scope in recipes_scopes:
+            item = RecipeItem()
+            item['name'] = r_scope.select(name_path).extract()
+            item['image'] = r_scope.select(image_path).extract()
+            item['url'] = r_scope.select(url_path).extract()
+            item['description'] = r_scope.select(description_path).extract()
+
+            item['prepTime'] = r_scope.select(prepTime_path).extract()
+            item['cookTime'] = r_scope.select(cookTime_path).extract()
+            item['recipeYield'] = r_scope.select(recipeYield_path).extract()
+
+            item['ingredients'] = r_scope.select(ingredients_path).extract()
+
+            item['datePublished'] = r_scope.select(datePublished).extract()
+
+            recipes.append(item)
+
+        return recipes
