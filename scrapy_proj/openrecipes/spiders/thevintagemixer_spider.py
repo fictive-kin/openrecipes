4997d728061aa8ce982d9d3252b7eae052bbd211
unknown <joga@BET-MT-JOGA.Betsson.local>
2013-04-03 15:10:30 +0200

    adding vintage mixer spiders, and fix in naturally ella spider

---
+++ scrapy_proj/openrecipes/spiders/thevintagemixer_spider.py
@@ -0,0 +1,8 @@
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
+from scrapy.selector import HtmlXPathSelector
+from openrecipes.items import RecipeItem
+
+
+class TheVintageMixerMixin(object):
+    print "hell"
