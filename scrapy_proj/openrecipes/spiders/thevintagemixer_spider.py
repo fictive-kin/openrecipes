611507cf1671d24e315291ecc86d22f4d2073c4f
Josef Galea <josefeg@gmail.com>
2013-04-03 16:32:58 +0200

    finished TheVintageSpiderMixin class...

--- scrapy_proj/openrecipes/spiders/thevintagemixer_spider.py
+++ scrapy_proj/openrecipes/spiders/thevintagemixer_spider.py
@@ -5,4 +5,46 @@ from openrecipes.items import RecipeItem
 
 
 class TheVintageMixerMixin(object):
-    print "hell"
+   
+    source = 'thevintagemixer'
+
+    def parse_item(self, response):
+
+        hxs = HtmlXPathSelector(response)
+
+        base_path = '//div[@itemtype="http://schema.org/Recipe"]'
+        recipes_scope = hxs.select(base_path)
+
+        ingredients_path = '//li[@itemprop="ingredients"]/text()';
+
+        image_path = '//div[@class="entry"]//img[1]/@src'
+        name_path = '//div[@itemprop="name"]/text()'
+        url_path = '//h2[@class="title"]/a/@href'
+
+        yield_path = '//span[@itemprop="servingSize"]/text()'
+        total_time_path = '//span[@itemprop="totalTime"]/@content'
+
+        recipes = []
+        for recipe_scope in recipes_scope:
+
+            item = RecipeItem()
+            item['source'] = self.source
+
+            item['image'] = recipe_scope.select(image_path).extract()
+            item['name'] = recipe_scope.select(name_path).extract()
+            item['url'] = recipe_scope.select(url_path).extract()
+
+            ingredients = []
+            ingredient_scopes = recipe_scope.select(ingredients_path)
+            for ingredient_scope in ingredient_scopes:
+                ingredient = ingredient_scope.extract().strip()
+                if (ingredient):
+                    ingredients.append(ingredient) 
+            item['ingredients'] = ingredients
+
+            item['recipeYield'] = recipe_scope.select(yield_path).extract()
+            item['totalTime'] = recipe_scope.select(total_time_path).extract()
+
+            recipes.append(item)
+
+        return recipes
