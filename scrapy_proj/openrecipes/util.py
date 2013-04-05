38e62b6785ac1024ec1c546c28196b0621061ef0
Evan Haas <evan@fictivekin.com>
2013-04-05 15:24:25 -0700

    remove cssselector and isodate dependencies for now

--- scrapy_proj/openrecipes/util.py
+++ scrapy_proj/openrecipes/util.py
@@ -1,32 +1,5 @@
-import isodate
-import datetime
-from lxml.cssselect import CSSSelector
-
-zeroInterval = datetime.timedelta(0)
-
-
 def parse_iso_date(scope):
     try:
-        return sum(
-            (isodate.parse_duration(time) for time in scope.select('@content | @datetime').extract()),
-            zeroInterval
-        )
+        return scope.select('@content | @datetime').extract()[0]
     except:
-        return scope.select('text()').extract()
-
-
-class JQ(object):
-    def __init__(self, root):
-        self.root = root
-
-    def select(self, selector):
-        return JQ(self.root.select(CSSSelector(selector).path))
-
-    def text(self):
-        return self.root.select('.//text()').extract()
-
-    def attr(self, name):
-        try:
-            return self.root[0].select('@' + name).extract()[0]
-        except IndexError:
-            return None
+        return ''
