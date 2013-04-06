3db779693980a05c06d6dd35402bb98732f489df
Justin Duke <justinduke@Justins-MacBook-Air.local>
2013-04-06 13:01:26 -0400

    Added spider for TastyKitchen

---
+++ scrapy_proj/openrecipes/spiders/tastykitchen_spider.py
@@ -0,0 +1,104 @@
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
+from scrapy.selector import HtmlXPathSelector
+from openrecipes.items import RecipeItem
+
+class TastyKitchenMixin(object):
+
+    """
+    Using this as a mixin lets us reuse the parse_item method more easily
+    """
+
+    # this is the source string we'll store in the DB to aggregate stuff
+    # from a single source
+    source = 'tastychicken'
+
+    def parse_item(self, response):
+
+        # we use this to run XPath commands against the HTML in the response
+        hxs = HtmlXPathSelector(response)
+
+        # this is the base XPath string for the element that contains the recipe
+        # info
+        base_path = """//div[@class="recipe-details"]"""
+
+        # the select() method will return a list of HtmlXPathSelector objects.
+        # On this site we will almost certainly either get back just one, if
+        # any exist on the page
+        recipes_scopes = hxs.select(base_path)
+
+        # it's easier to define these XPath strings outside of the loop below
+        name_path = '//h1[@itemprop="name"]/text()'
+        recipeYield_path = '//label[@for="set_servings"]/input/@value'
+        description_path = '//span[@itemprop="summary"]/p/text()'
+        image_path = '//img[@class="the_recipe_image"]/@src'
+        cookTime_path = '//form/p/time[@itemprop="cookTime"]/text()'
+        prepTime_path = '//form/p/time[@itemprop="prepTime"]/text()'
+        ingredients_path = '//span[@itemprop="ingredient"]'
+        ingredients_amounts_path = './span[@itemprop="amount"]/text()'
+        ingredients_names_path = './span[@itemprop="amount"]/text()'
+
+        # init an empty list
+        recipes = []
+
+        # loop through our recipe scopes and extract the recipe data from each
+        for r_scope in recipes_scopes:
+            # make an empty RecipeItem
+            item = RecipeItem()
+
+            item['source'] = self.source
+
+            item['name'] = r_scope.select(name_path).extract()
+            item['image'] = r_scope.select(image_path).extract()
+            item['description'] = r_scope.select(description_path).extract()
+            item['url'] = response.url
+            item['prepTime'] = r_scope.select(prepTime_path).extract()
+            item['cookTime'] = r_scope.select(cookTime_path).extract()
+            item['recipeYield'] = r_scope.select(recipeYield_path).extract()
+
+            ingredient_scopes = r_scope.select(ingredients_path)
+            amount = ingredient_scopes.select(ingredients_amounts_path).extract()
+            name = ingredient_scopes.select(ingredients_names_path).extract()
+            ingredients = [" ".join(ing).encode('utf-8') for ing in zip(amount, name)]
+
+            item['ingredients'] = ingredients
+
+            # stick this RecipeItem in the array of recipes we will return
+            recipes.append(item)
+
+        # more processing is done by the openrecipes.pipelines. Look at that
+        # file to see transforms that are applied to each RecipeItem
+        return recipes
+
+
+class TastyKitchenSpider(CrawlSpider, TastyKitchenMixin):
+
+    # this is the name you'll use to run this spider from the CLI
+    name = "tastykitchen.com"
+
+    # URLs not under this set of domains will be ignored
+    allowed_domains = [
+        "tastykitchen.com"
+    ]
+
+    # the set of URLs the crawler will start from
+    start_urls = [
+        "http://tastykitchen.com/recipes/page/1/"
+    ]
+
+    # a tuple of Rules that are used to extract links from the HTML page
+    rules = (
+        # this rule has no callback, so these links will be followed and mined
+        # for more URLs. This lets us page through the recipe archives
+        Rule(SgmlLinkExtractor(allow=('recipes/page/\d+'))),
+
+        # this rule is for recipe posts themselves. The callback argument will
+        # process the HTML on the page, extract the recipe information, and
+        # return a RecipeItem object
+        # Added $ to prevent duplicates via referral arguments
+        Rule(SgmlLinkExtractor(
+            allow=('/recipes/.+'),
+            restrict_xpaths=('//h2')
+        ),
+        callback='parse_item'),
+    )
