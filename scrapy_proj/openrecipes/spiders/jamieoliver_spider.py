ee4185d4ed16d821ccba7ae05cc8f71bf823ff53
Ed Finkler <coj@funkatron.com>
2013-04-14 16:48:58 -0400

    convert jamieoliver to use RecipeItemLoader

--- scrapy_proj/openrecipes/spiders/jamieoliver_spider.py
+++ scrapy_proj/openrecipes/spiders/jamieoliver_spider.py
@@ -42,27 +42,27 @@ class JamieoliverMixin(object):
         # loop through our recipe scopes and extract the recipe data from each
         for r_scope in recipes_scopes:
             # make an empty RecipeItem
-            item = RecipeItem()
+            il = RecipeItemLoader(item=RecipeItem())
 
-            item['source'] = self.source
+            il.add_value('source', self.source)
 
-            item['name'] = r_scope.select(name_path).extract()
-            item['image'] = urljoin(response.url, r_scope.select(image_path).extract().pop(0))
-            item['url'] = response.url
-            item['description'] = r_scope.select(description_path).extract()
+            il.add_value('name', r_scope.select(name_path).extract())
+            il.add_value('image', urljoin(response.url, r_scope.select(image_path).extract().pop(0)))
+            il.add_value('url', response.url)
+            il.add_value('description', r_scope.select(description_path).extract())
 
             # prepTime not available
-            item['prepTime'] = None
+            il.add_value('prepTime', None)
             # cookTime not available
-            item['cookTime'] = None
-            item['recipeYield'] = r_scope.select(recipeYield_path).extract()
-            item['ingredients'] = r_scope.select(ingredients_path).extract()
+            il.add_value('cookTime', None)
+            il.add_value('recipeYield', r_scope.select(recipeYield_path).extract())
+            il.add_value('ingredients', r_scope.select(ingredients_path).extract())
 
             # datePublished not available
-            item['datePublished'] = None
+            il.add_value('datePublished', None)
 
             # stick this RecipeItem in the array of recipes we will return
-            recipes.append(item)
+            recipes.append(il.load_item())
 
         # more processing is done by the openrecipes.pipelines. Look at that
         # file to see transforms that are applied to each RecipeItem
