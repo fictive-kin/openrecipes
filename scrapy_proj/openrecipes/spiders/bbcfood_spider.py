22d57ba7693c02c0c2b25e367617b1e928835e99
Ed Finkler <coj@funkatron.com>
2013-04-14 16:31:40 -0400

    convert bbcfood to use RecipeItemLoader

--- scrapy_proj/openrecipes/spiders/bbcfood_spider.py
+++ scrapy_proj/openrecipes/spiders/bbcfood_spider.py
@@ -26,18 +26,18 @@ class BBCfoodMixin(object):
         recipes = []
 
         for r_scope in recipes_scopes:
-            item = RecipeItem()
+            il = RecipeItemLoader(item=RecipeItem())
 
-            item['source'] = self.source
+            il.add_value('source', self.source)
 
-            item['name'] = r_scope.select(name_path).extract()
-            item['image'] = r_scope.select(image_path).extract()
-            item['url'] = response.url
-            item['description'] = r_scope.select(description_path).extract()
+            il.add_value('name', r_scope.select(name_path).extract())
+            il.add_value('image', r_scope.select(image_path).extract())
+            il.add_value('url', response.url)
+            il.add_value('description', r_scope.select(description_path).extract())
 
-            item['prepTime'] = r_scope.select(prepTime_path).extract()
-            item['cookTime'] = r_scope.select(cookTime_path).extract()
-            item['recipeYield'] = r_scope.select(recipeYield_path).extract()
+            il.add_value('prepTime', r_scope.select(prepTime_path).extract())
+            il.add_value('cookTime', r_scope.select(cookTime_path).extract())
+            il.add_value('recipeYield', r_scope.select(recipeYield_path).extract())
 
             ingredient_scopes = r_scope.select(ingredients_path)
             ingredients = []
@@ -47,9 +47,9 @@ class BBCfoodMixin(object):
                 amount = "".join(amount).strip()
                 name = "".join(name).strip()
                 ingredients.append("%s %s" % (amount, name))
-            item['ingredients'] = ingredients
+            il.add_value('ingredients', ingredients)
 
-            recipes.append(item)
+            recipes.append(il.load_item())
 
         return recipes
 
