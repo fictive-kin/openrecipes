6591b959d1a47ed5866cc28979a2e58ba5fdff8c
Ed Finkler <coj@funkatron.com>
2013-04-13 22:52:03 -0400

    use date_str instead of input as arg name

--- scrapy_proj/openrecipes/util.py
+++ scrapy_proj/openrecipes/util.py
@@ -20,27 +20,27 @@ def flatten(list_or_string):
         return list_or_string
 
 
-def get_isodate(input):
-    """convert the given input string into an iso 8601 date"""
 def strip_html(html_str):
     """removes all HTML markup using the Bleach lib"""
     return bleach.clean(html_str, tags=[], attributes={},
                                    styles=[], strip=True)
 
 
+def get_isodate(date_str):
+    """convert the given date_str string into an iso 8601 date"""
     iso_date = None
 
-    if not input:
+    if not date_str:
         return None
 
     #first, is it already a valid isodate?
     try:
-        isodate.parse_date(input)
-        return input
+        isodate.parse_date(date_str)
+        return date_str
     except isodate.ISO8601Error, e:
         # if not, try to parse it
         try:
-            iso_date = isodate.date_isoformat(parse(input))
+            iso_date = isodate.date_isoformat(parse(date_str))
         except Exception, e:
             log.msg(e.message, level=log.WARNING)
             return None
@@ -48,21 +48,21 @@ def strip_html(html_str):
         return iso_date
 
 
-def get_isoduration(input):
-    """convert the given input string into an iso 8601 duration"""
+def get_isoduration(date_str):
+    """convert the given date_str string into an iso 8601 duration"""
     iso_duration = None
 
-    if not input:
+    if not date_str:
         return None
 
     #first, is it already a valid isoduration?
     try:
-        isodate.parse_duration(input)
-        return input
+        isodate.parse_duration(date_str)
+        return date_str
     except isodate.ISO8601Error, e:
         # if not, try to parse it
         try:
-            delta = (parse(input) - parse(''))
+            delta = (parse(date_str) - parse(''))
             iso_duration = isodate.duration_isoformat(delta)
         except Exception, e:
             log.msg(e.message, level=log.WARNING)
