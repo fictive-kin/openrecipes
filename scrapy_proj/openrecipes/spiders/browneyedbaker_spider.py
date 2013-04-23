4f578180e0a24be01b6615cd365897d80e10ee79
Ed Finkler <coj@funkatron.com>
2013-04-23 16:25:37 -0400

    PEP8 fix

--- scrapy_proj/openrecipes/spiders/browneyedbaker_spider.py
+++ scrapy_proj/openrecipes/spiders/browneyedbaker_spider.py
@@ -1,7 +1,8 @@
 from scrapy.contrib.spiders import CrawlSpider, Rule
 from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 from scrapy.selector import HtmlXPathSelector
-from openrecipes.items import RecipeItem, RecipeItemLoader 
+from openrecipes.items import RecipeItem, RecipeItemLoader
+
 
 class BrownEyedBakerMixin(object):
     source = 'browneyedbaker'
