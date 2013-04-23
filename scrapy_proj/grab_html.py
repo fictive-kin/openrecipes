b8048287be752e03b68dddc868bcacdf4f519d26
Ed Finkler <coj@funkatron.com>
2013-04-23 17:06:48 -0400

    remove commented code

--- scrapy_proj/grab_html.py
+++ scrapy_proj/grab_html.py
@@ -31,38 +31,6 @@ def grab_html(args):
     filename = os.path.join(htmldir, title + '.html')
     with open(filename, 'w') as f:
         f.write(lxml.etree.tostring(t, pretty_print=True))
-    # print response.content
-
-    # parsed_url = parse_url(args.start_url)
-
-    # domain = parsed_url.netloc
-    # name = args.name.lower()
-
-    # values = {
-    #     'crawler_name': name.capitalize(),
-    #     'source': name,
-    #     'name': domain,
-    #     'domain': domain,
-    #     'start_url': args.start_url,
-    # }
-
-    # spider_filename = os.path.join(script_dir, 'openrecipes', 'spiders', '%s_spider.py' % name)
-    # with open(spider_filename, 'w') as f:
-    #     f.write(SpiderTemplate % values)
-
-    # if args.with_feed:
-    #     feed_url = args.with_feed[0]
-    #     feed_domain = parse_url(feed_url).netloc
-    #     values['feed_url'] = feed_url
-    #     values['name'] = name
-    #     if feed_domain == domain:
-    #         values['feed_domains'] = domain
-    #     else:
-    #         values['feed_domains'] = '%s",\n        "%s' % (domain, feed_domain)
-    #     feed_filename = os.path.join(script_dir, 'openrecipes', 'spiders', '%s_feedspider.py' % name)
-    #     with open(feed_filename, 'w') as f:
-    #         f.write(FeedSpiderTemplate % values)
-
         print "Wrote %s" % filename
 
 epilog = """
